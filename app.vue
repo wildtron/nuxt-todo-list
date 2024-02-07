f368b9a19e3729ab3c79e516fa59fc6b1d74ac2d
Sherwin Ferrer <sherwin.ferrer@freedom.tm>
2024-02-08 01:11:42 +0800

    feat: basic todo

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+    <UContainer class="max-w-screen-sm mx-auto mt-64">
+        <UCard>
+            <template #header>
+                List
+            </template>
+            <UInput
+                v-model="input"
+                @keypress="insert_todo"
+            />
+            <UDivider
+                label="LIST"
+                class="py-4"
+            />
+            <div
+                v-for="todo in todo_list"
+            >
+                <p>{{ todo }}</p>
+            </div>
+        </UCard>
+    </UContainer>
+</template>
+
+<script setup lang="ts">
+    const input = ref('');
+    const todo_list = ref(<string[]>[]);
+
+    function insert_todo (e) {
+
+        // user presses enter
+        if (e.keyCode === 13) {
+            todo_list.value.push(input.value);
+
+            input.value = '';
+        }
+
+    }
+</script>
